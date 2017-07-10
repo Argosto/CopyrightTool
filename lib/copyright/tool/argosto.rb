3b1aa50888bc22b58957c42d313b71d5c4b0a762
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:38:40 +0000

    first commit

---
+++ lib/copyright/tool/argosto.rb
@@ -0,0 +1,9 @@
+require "copyright/tool/argosto/version"
+
+module Copyright
+  module Tool
+    module Argosto
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/copyright/tool/argosto/version.rb
@@ -0,0 +1,7 @@
+module Copyright
+  module Tool
+    module Argosto
+      VERSION = "0.1.0"
+    end
+  end
+end
